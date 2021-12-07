a23a788d6386aed6f2795e0cf6c86361c7cdb949
Marcos Sierra <marcosjspac@gmail.com>
2021-12-07 13:05:47 -0600

    agregando los componentes qeu se necesitaran

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
+import React from 'react';
+import { TodoCounter } from './TodoCounter';
+import { TodoItem } from './TodoItem';
+import { TodoSearch } from './TodoSearch';
+import { CreateTodoButton } from './CreateTodoButton';
+import { TodoList } from './TodoList';
 // import './App.css';
 
+const todos = [
+  { text: 'Cortar cebolla', completed: false},
+  { text: 'Tomar el curso de intro', completed: false},
+  { text: 'Llorar con la llorona', completed: false}
+];
 
 function App(props) {
   return (
-    <TodoCounter />
-    <TodoSearch />
-    <TodoList>
-      <TodoItem />
-    </TodoList>
-    <CreateTodoButton />
+    <React.Fragment>
+      <TodoCounter />
+      <TodoSearch />
+      <TodoList>
+        {todos.map(todo => (
+          <TodoItem key={todo.text} text={todo.text} />
+        ))}
+      </TodoList>
+      <CreateTodoButton />
+      <button>+</button>
+    </React.Fragment>
   );
 }
 
