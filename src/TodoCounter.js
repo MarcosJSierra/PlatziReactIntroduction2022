854dcbe9d51c935fc41393979a1e46861a838f69
Marcos Sierra <marcosjspac@gmail.com>
2021-12-14 12:30:39 -0600

    agregando css

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import './TodoCounter.css';
+
+const estilos = {
+    color: 'red',
+    backroundColor: 'yellow',
+};
 
 function TodoCounter(){
     return (
-        <h2>Has comletado 2 de 3 TODOs</h2>
+        <h2 className="TodoCounter" >Has comletado 2 de 3 TODOs</h2>
     );
 }
 
